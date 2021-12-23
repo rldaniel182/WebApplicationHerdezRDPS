2d800d54e1209543053e78b684a226014a9c807f
rldaniel182 <rldaniel182@gmail.com>
2021-12-23 01:16:22 -0600

    Agregar archivos de proyecto.

---
+++ wwwroot/js/App/Productos.js
@@ -0,0 +1,137 @@
+﻿// :::::::::::::::--  BUSCA PRODUCTO  --::::::::::::::::::::
+
+$('#btnBuscarProd').click(function () {
+    event.preventDefault();
+    ValidateFormBuscaProducto();
+    if ($('#frmBuscaProducto').valid()) {
+        BuscaProducto();
+    }
+});
+
+function ValidateFormBuscaProducto() {
+    $('#frmBuscaProducto').validate({
+        rules: {
+            txbBuscarProd: {
+                required: true
+            }
+        },
+        errorElement: "em",
+        errorPlacement: function (error, element) {
+            error.addClass("help-block");
+            if (element.prop("type") === "checkbox") {
+                error.insertAfter(element.parent("label"));
+            } else {
+                error.insertAfter(element);
+            }
+        },
+        highlight: function (element, errorClass, validClass) {
+        },
+        unhighlight: function (element, errorClass, validClass) {
+        }
+    });
+}
+
+function BuildObjectBuscarProdcuto() {
+    let Data = new FormData();
+    Data.append("Clave", $('#txbBuscarProd').val());
+    return Data;
+}
+
+function BuscaProducto() {
+    event.preventDefault();
+    let AjaxPath = SysHost.Uri + 'Producto/GetProducto';
+    let Parameters = BuildObjectBuscarProdcuto();
+    CallAsyncObjectGenericWhitFiles(
+        AjaxPath,
+        Parameters,
+        function functionSuccessCallBack(xData) {
+            var response = xData.data;
+            console.log(response);
+            if (response.idResponse === 1) {
+                PutInformationInModalProducto(response.objectGeneric);
+            }
+            else {
+                alert(response.body);
+            }
+        }
+    );
+}
+
+function PutInformationInModalProducto(Producto) {
+    $('#claveProducto').val(Producto.claveProducto);
+    $('#nombreProducto').val(Producto.nombreProducto);
+    $('#distancia').val(Producto.distancia);
+    $('#precioPesos').val(Producto.precioPesos);
+    $('#iva').val(Producto.iva);
+    $('#totalEnvioDolares').val(Producto.totalEnvioDolares);
+    $('#totalEnvioEuros').val(Producto.totalEnvioEuros);
+    $("#frmBuscaProducto").validate().destroy();
+    $('#frmBuscaProducto')[0].reset();
+    $('#ModalProducto').modal('show');
+}
+
+// :::::::::::::::--  BUSCA PRODUCTOS  --::::::::::::::::::::
+
+// LEFTH
+$('#btnBuscarProdLeft').click(function () {
+    event.preventDefault();
+    BuscaProductosLeft();
+});
+
+function BuscarProdLeft() {
+    let AjaxPath = SysHost.Uri + 'Producto/GetLefth';
+    CallAsyncObjectGenericGET(
+        AjaxPath,
+        function functionSuccessCallBack(xData) {
+            let response = xData.data;
+            console.log(response);
+            if (response.idResponse === 1) {
+                datatableInitColumArray('tblProdcutos', response.objectGeneric);
+            }
+            else {
+                alert(response.body);
+                datatableInitColumArray('tblProdcutos', null);
+            }
+        }
+    );
+}
+
+// RIGTH
+
+$('#btnBuscarProdRigth').click(function () {
+    event.preventDefault();
+    BuscaProductosRigth();
+});
+
+function BuscarProdRight() {
+    let AjaxPath = SysHost.Uri + 'Producto/GetRigth';
+    CallAsyncObjectGenericGET(
+        AjaxPath,
+        function functionSuccessCallBack(xData) {
+            let response = xData.data;
+            console.log(response);
+            if (response.idResponse === 1) {
+                datatableInitColumArray('tblProdcutos', response.objectGeneric);
+            }
+            else {
+                alert(response.body);
+                datatableInitColumArray('tblProdcutos', null);
+            }
+        }
+    );
+}
+
+// :::::::::::::::--  DATATABLE  --::::::::::::::::::::
+
+function datatableInitColumArray(IdDataTable, DataList) {
+    //Se crea un array con las columnas data -> mDataProp
+    let columnArray = new Array();
+
+    // General Columns 
+    columnArray.push({ "sTitle": "Clave", "mDataProp": "Clave", "sClass": "text-left", "sWidth": "20%", "bSortable": true });
+    columnArray.push({ "sTitle": "Nombre", "mDataProp": "Nombre", "sClass": "text-left", "sWidth": "20%", "bSortable": true });
+    DataTableGeneric(IdDataTable, DataList, columnArray);
+    $("#" + IdDataTable + "").DataTable().columns.adjust();
+
+}
+
---
+++ wwwroot/js/site.js
@@ -0,0 +1,195 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+// :::::::::::::::--Source DataTable --::::::::::::::::::::::::::::::
+
+function DataTableGeneric(IdDataTable, DataList, columnArray) {
+    let IsDataTableWasCreated = $.fn.DataTable.isDataTable('#' + IdDataTable);
+    if (IsDataTableWasCreated) {
+        $('#' + IdDataTable).DataTable().destroy();
+        $('#' + IdDataTable).empty();
+    }
+    $('#' + IdDataTable).DataTable({
+        "bDestroy": true
+        , "scrollY": "300px"
+        , "scrollCollapse": true
+        , "paging": true
+        , "autoWidth": true
+        , "compact": true
+        , "scrollX": false
+        , "aaSorting": [[0, "asc"]]
+        , data: DataList
+        , "aoColumns": columnArray
+        , "fnInitComplete": function (settings, json) {
+            setTimeout(function () {
+                $($.fn.dataTable.tables(true)).DataTable().columns.adjust().draw();
+            }, 100);
+        }
+    }).columns.adjust();
+};
+// :::::::::::::::--AJAX--::::::::::::::::::::::::::::::::::::::::::::::::::::
+var SysHost = {
+    Uri: $('#Host').val()
+};
+
+function CallAsyncObjectGenNoLoading(url, xdata, functionSuccessCallBack) {
+
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: { xdata: xdata },
+        dataType: 'json',
+        beforeSend: function () {
+            console.log("initiating connection to the server");
+        },
+        success: function (xData) {
+            functionSuccessCallBack(xData);
+        },
+        error: function (xhr, status, error) {
+            console.log(error);
+            alert("The operation cannot be completed at this time, please try again later");
+        },
+        complete: function () {
+            console.log("End connection to server");
+        }
+    });
+}
+
+function CallAsyncGETNoLoading(url, xdata, functionSuccessCallBack) {
+
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: { xdata: xdata },
+        dataType: 'json',
+        beforeSend: function () {
+            console.log("initiating connection to the server");
+        },
+        success: function (xData) {
+            functionSuccessCallBack(xData);
+        },
+        error: function (xhr, status, error) {
+            console.log(error);
+            alert("The operation cannot be completed at this time, please try again later");
+        },
+        complete: function () {
+            console.log("End connection to server");
+        }
+    });
+}
+
+function CallAsyncObjectGenericWhitFiles(url, xdata, functionSuccessCallBack) {
+
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: xdata,
+        processData: false,
+        contentType: false,
+        cache: false,
+        beforeSend: function () {
+            $('#ModalLoading').modal('show');
+            console.log("initiating connection to the server");
+        },
+        success: function (xData) {
+            functionSuccessCallBack(xData);
+        },
+        error: function (xhr, status, error) {
+            console.log(error);
+            alert("The operation cannot be completed at this time, please try again later");
+            $('#ModalLoading').modal('hide');
+        },
+        complete: function () {
+            console.log("End connection to server");
+            $('#ModalLoading').modal('hide');
+
+        }
+    });
+}
+
+function CallAsyncObjectGeneric(url, xdata, functionSuccessCallBack) {
+
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: { xdata: xdata },
+        dataType: 'json',
+        beforeSend: function () {
+            console.log("initiating connection to the server");
+            $('#ModalLoading').modal('show');
+        },
+        success: function (xData) {
+            functionSuccessCallBack(xData);
+        },
+        error: function (xhr, status, error) {
+            alert("The operation cannot be completed at this time, please try again later");
+            console.log(error);
+            $('#ModalLoading').modal('hide');
+        },
+        complete: function () {
+            console.log("End connection to server");
+            $('#ModalLoading').modal('hide');
+        }
+    });
+}
+
+function CallAsyncObjectGenericGET(url, functionSuccessCallBack) {
+
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'json',
+        beforeSend: function () {
+            console.log("initiating connection to the server");
+            $('#ModalLoading').modal('show');
+        },
+        success: function (xData) {
+            functionSuccessCallBack(xData);
+        },
+        error: function (xhr, status, error) {
+            alert("The operation cannot be completed at this time, please try again later");
+            console.log(error);
+            $('#ModalLoading').modal('hide');
+        },
+        complete: function () {
+            console.log("End connection to server");
+            $('#ModalLoading').modal('hide');
+        }
+    });
+}
+
+function CallAsyncObjectGenericDDL(url, xdata, functionSuccessCallBack) {
+
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'json',
+        beforeSend: function () {
+            console.log("initiating connection to the server");
+        },
+        success: function (xData) {
+            functionSuccessCallBack(xData);
+        },
+        error: function (xhr, status, error) {
+            console.log(error);
+            alert("The operation cannot be completed at this time, please try again later");
+        },
+        complete: function () {
+            console.log("End connection to server");
+        }
+    });
+}
+
+// :::::::::::::::--Others--::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+function LoadDropdownList(data, IdTag) {
+    let long = data.length;
+    for (var i = 0; i < long; i++) {
+        $(IdTag).append($("<option />").val(data[i].idArea).text(data[i].name));
+    }
+}
